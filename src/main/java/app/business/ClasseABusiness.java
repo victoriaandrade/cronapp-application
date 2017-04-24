56bcec9cc1c61b461cb33017e2cf51aaba824683
victoriaandrade <victoriaandrade>
2017-04-24 18:49:12 +0000

    Classe nova adicionada

---
+++ src/main/java/app/business/ClasseABusiness.java
@@ -0,0 +1,23 @@
+package app.business;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author victoria.andrade@techne.com.br
+ * @version 1.0
+ * @since 2017-04-24
+ *
+ */
+ 
+public class ClasseABusiness {
+    public void teste() {
+      
+    }
+	/**
+	 * Construtor
+	 **/
+	public ClasseABusiness (){
+	}
+
+}
